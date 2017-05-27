9898ab121897f5165051c7ade48e97fdb4548ee2
dtepdc <dawn714@gmail.com>
2017-05-27 13:18:12 -0400

    update files for Headlines App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
