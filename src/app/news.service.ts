22b45c15e63157babd557d83fcb12b2d448ef847
dtepdc <dawn714@gmail.com>
2017-05-27 21:18:11 -0400

    add service file(s)

---
+++ src/app/news.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class NewsService {
+  constructor (
+    private http: Http
+  ) {}
+
+  getNews() {
+    return this.http.get(`https://newsapi.org/v1/articles?source=the-next-web&apiKey=5db44d48109b427aaf891efcd542b908`)
+    .map((res:Response) => res.json());
+  }
+
+}
